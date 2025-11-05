0f1b14fb5d6237a12491a97e10b8f2363f30b720
Robin Wydaeghe <robin.wydaeghe@ugent.be>
2025-11-05 21:25:28 +0100

    Add migration for warningCount and errorCount columns

---
+++ prisma/migrations/20251105202200_add_warning_error_counts/migration.sql
@@ -0,0 +1,6 @@
+-- AlterTable
+ALTER TABLE "gui_state" ADD COLUMN "warningCount" INTEGER NOT NULL DEFAULT 0;
+
+-- AlterTable
+ALTER TABLE "gui_state" ADD COLUMN "errorCount" INTEGER NOT NULL DEFAULT 0;
+
